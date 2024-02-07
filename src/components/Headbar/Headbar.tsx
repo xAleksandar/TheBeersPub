65a5fbbb1a1166fad6e031d8931eec8a708bd165
aleks <aleksandar@kalaydzhiev.com>
2024-02-07 22:19:52 +0200

    Improvements

--- src/components/Headbar/Headbar.tsx
+++ src/components/Headbar/Headbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import { ClickableDiv } from "../";
 import { lang } from "../../lib/language";
 import { useNavigate } from "react-router-dom";
@@ -10,19 +11,25 @@ const Headbar = () => {
   const navigate = useNavigate();
   const name = "TheBeersPub";
 
+  const isActiveTab = (route: string) => window.location.pathname === route;
+
   return (
     <div className={styles.wrapperHeadbar}>
       <div className={styles.name}>{name}</div>
       <div className={styles.options}>
         <ClickableDiv
           onClick={() => navigate(routesEnum.home)}
-          className={styles.option}
+          className={classNames(styles.option, {
+            [styles.activeOption]: isActiveTab(routesEnum.home),
+          })}
         >
           {lang.commonHome}
         </ClickableDiv>
         <ClickableDiv
           onClick={() => navigate(routesEnum.favourites)}
-          className={styles.option}
+          className={classNames(styles.option, {
+            [styles.activeOption]: isActiveTab(routesEnum.favourites),
+          })}
         >
           {lang.commonFavourites}
         </ClickableDiv>
